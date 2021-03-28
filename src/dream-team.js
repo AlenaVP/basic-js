587467c8999740d3d932eef274284a77f0520282
AlenaVP <poprugo@gmail.com>
2021-03-28 10:57:37 +0300

    feat: add a solution of 'Dream team' task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !Array.isArray(members)) {
+    return false;
+  }
+  const letters = [];
+  for (const name of members) {
+    if (typeof name !== 'string') {
+      continue;
+    } else {
+      letters.push(name.trim().charAt(0).toUpperCase());
+    }
+  }
+  return letters.sort().join('');
 };
