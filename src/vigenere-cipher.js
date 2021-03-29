8a2167f5d9f3e8566c8ad873238cf50707add0c5
AlenaVP <poprugo@gmail.com>
2021-03-30 00:15:01 +0300

    feat: add a solution of 'Vigenere cipher' task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    const chars = message.toUpperCase().split('').map(c => c.charCodeAt());
+    const wordChars = key.toUpperCase().repeat(Math.ceil(message.length / key.length)).split('').map(c => c.charCodeAt());
+    const encrypted = Array.from({length: chars.length}, (_, i) => 65 > chars[i] || chars[i] > 90 ? String.fromCharCode(chars[i])
+      : String.fromCharCode((chars[i] + wordChars.shift()) % 26 + 65));
+    return this.direct === false ? encrypted.reverse().join('') : encrypted.join('');
+  }
+  decrypt(encryptedMessage, key) {
+    const chars = encryptedMessage.toUpperCase().split('').map(c => c.charCodeAt());
+    const wordChars = key.toUpperCase().repeat(Math.ceil(encryptedMessage.length / key.length)).split('').map(c => c.charCodeAt());
+    const decrypted = Array.from({length: chars.length}, (_, i) => 65 > chars[i] || chars[i] > 90 ? String.fromCharCode(chars[i])
+      : String.fromCharCode((chars[i] + 26 - wordChars.shift()) % 26 + 65));
+    return this.direct === false ? decrypted.reverse().join('') : decrypted.join('');
   }
 }
 
