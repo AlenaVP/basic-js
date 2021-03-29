aa107bb1fe299f658eb39465328b2a19c70e9b9b
AlenaVP <poprugo@gmail.com>
2021-03-29 12:07:32 +0300

    feat: add a solution of 'Recursive depth calculator' task
    all tests passed

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    return Array.isArray(arr) ? 1 + this.calculateDepth(...arr.filter(a => Array.isArray(a))) : 0;
+    return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map(a => this.calculateDepth(a))) : 0;
   }
 };
